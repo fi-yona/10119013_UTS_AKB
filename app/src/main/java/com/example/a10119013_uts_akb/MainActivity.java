76837a363a47f0e4afff3bca8e16930301294bd9
fi-yona <fionaavila15@gmail.com>
2022-06-05 18:46:16 +0700

    add function intent from splash screen to home

--- app/src/main/java/com/example/a10119013_uts_akb/MainActivity.java
+++ app/src/main/java/com/example/a10119013_uts_akb/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.a10119013_uts_akb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_mulai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //inisialisasi
+        button_mulai = findViewById(R.id.button_mulai);
+
+        button_mulai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ContentActicity.class);
+                startActivity(i);
+            }
+        });
     }
 }
